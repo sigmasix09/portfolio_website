e80814a26e40c7a965d97b7ae70bebcf5b226f17
Abhishek Dhiman <ateamas09@gmail.com>
2020-04-28 12:41:12 +0530

    Portfolio website updated successfully.

---
+++ web_server.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, redirect
+import csv
+# instantiating flask app instance, setting name as main
+app = Flask(__name__)
+print(__name__)
+
+# web_server.py file information
+'''
+This script is for web development purpose.
+It works on 3 basic files: html(text on te web page), CSS(styling), JS(behaviour/actions)
+When browser make request, the server send the 3 files to it and display the request web page.
+The server can be coded using python.
+html, css, js files received from server is of type document, css and js respectively.
+Building Server using 'FLASK' framework
+render_template: allows user to send the html file
+template folder: to store only html files.
+static folder: to store css adn js file
+favicon: {{ python expression }} is jinja templating
+'''
+
+# web_server.py additional information
+'''
+Anytime we enter '/' (root directory) defines a function hello_world() and returns the value
+Export flask: set FLASK_APP=web_server.py (name of the server)
+To run: flask run
+Standard Local host: http://127.0.0.1:5000/
+set FLASK_ENV=deployment, to activate debugger so that changes can reflect on refresh
+'''
+
+# 1st file's decorators
+'''
+@app.route('/<username>/<int:age>')
+def hello_world(username=None, age=None):
+    return render_template('text.html', name=username, age=age)
+
+
+@app.route('/details/<username>/<int:age>')
+def my_details(username=None, age=None):
+    return render_template('details.html', name=username, age=age)
+'''
+
+'''
+calling all the pages
+'''
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def web_name(page_name):
+    return render_template(page_name)
+
+@app.route('/contact_form', methods=['POST', 'GET'])
+def contact_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong, please try again later.'
+
+
+'''
+This function writes the details of contact form into a csv file. Add a new line in the csv file.
+Github account: ateamas; for storing these files.
+PythonAnywhere: sigmasix; for creating the website.
+'''
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as f1:
+        email = data["Email"]
+        subject = data["Subject"]
+        message = data["Message"]
+        csv_writer = csv.writer(f1, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
