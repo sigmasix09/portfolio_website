847e732a4d58990098e6be2df5865f21c487a25d
ABHISHEK DHIMAN <ateamas09@gmail.com>
2021-09-14 12:50:29 +0530

    file updated

--- web_server.py
+++ web_server.py
@@ -81,13 +81,7 @@ def contact_form():
     if request.method == 'POST':
         data = request.form.to_dict()
         write_to_csv(data)
-        print('written to csv')
-<<<<<<< HEAD
         send_data_to_owner(data)
-=======
-        # send_data_to_owner(data)
->>>>>>> 7cc90294456dfa9e906020276a97fb22fe9da3ef
-        print('sent to owner')
         return redirect('/thankyou.html')
     else:
         return 'Something went wrong, please try again later.'
@@ -116,7 +110,6 @@ def write_to_csv(data):
 This function takes the visitor's entered data and send
 an SMS to the owner with the data.
 '''
-<<<<<<< HEAD
 def send_data_to_owner(data):
     print('Abhishek')
     message_item = [time.asctime(time.localtime()), data["Email"], data["Subject"], data["Message"]]
@@ -124,9 +117,7 @@ def send_data_to_owner(data):
         account_sid = os.getenv('ACCOUNT_SID')
         auth_token = os.getenv('AUTH_TOKEN')
         proxy_client = TwilioHttpClient(proxy={'http': os.environ['http_proxy'], 'https': os.environ['https_proxy']})
-        print(os.environ['HTTP_PROXY'])
         client = Client(account_sid, auth_token, http_client = proxy_client)
-        print(os.environ['HTTPS_PROXY'])
         message = client.messages.create(
             from_ = '+15202140910',
             body = str(message_item),
@@ -136,24 +127,3 @@ def send_data_to_owner(data):
         print("Executed Successfully.")
     except TwilioRestException as e:
         print(e)
-=======
-# def send_data_to_owner(data):
-#     print('Abhishek')
-#     message_item = [time.asctime(time.localtime()), data["Email"], data["Subject"], data["Message"]]
-#     try:
-#         account_sid = os.getenv('ACCOUNT_SID')
-#         auth_token = os.getenv('AUTH_TOKEN')
-#         proxy_client = TwilioHttpClient(proxy={'http': os.environ['http_proxy'], 'https': os.environ['https_proxy']})
-#         print(os.environ['HTTP_PROXY'])
-#         client = Client(account_sid, auth_token, http_client = proxy_client)
-#         print(os.environ['HTTPS_PROXY'])
-#         message = client.messages.create(
-#             from_ = '+15202140910',
-#             body = str(message_item),
-#             to = '+919662667244'
-#         )
-#         print(message.sid)
-#         print("Executed Successfully.")
-#     except TwilioRestException as e:
-#         print(e)
->>>>>>> 7cc90294456dfa9e906020276a97fb22fe9da3ef
