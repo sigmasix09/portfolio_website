8021670e005e0be22f291a109c7c3e383fe93dd8
sigmasix09 <sigmasix09@gmail.com>
2024-07-02 13:56:58 +0000

    commit1_thesis

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class AppConfig:
     ACCOUNT_SID = "--AC7c0934ae4094bec--ef697671008746641--"
     AUTH_TOKEN = "--733a5312aec1c8a03--648c98f68a64c7a--"
-    sender_num = '+12295455284'
+    sender_num = '+15203694341'
     receiver_num = '+919662667244'
