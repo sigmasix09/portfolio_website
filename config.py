23fadc97792203ef72e094fe86731a311b6aec52
ABHISHEK DHIMAN <ateamas09@gmail.com>
2025-06-04 19:50:10 +0200

    commit1_04062025

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class AppConfig:
-    ACCOUNT_SID = "--AC7c0934ae4094bec--ef697671008746641--"
-    AUTH_TOKEN = "--733a5312aec1c8a03--648c98f68a64c7a--"
-    sender_num = '+15203694341'
+    ACCOUNT_SID = "--AC61c824d80952987--e71aed9b7a4f4e209--"
+    AUTH_TOKEN = "--f42f0249f8baed65--0a1ca4a4f91ac5fe--"
+    sender_num = '+14139377353'
     receiver_num = '+919662667244'
